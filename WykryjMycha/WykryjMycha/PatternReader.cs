2a5dfd0fe3a35802baed1fa4487e5dc2aac87b12
dc064 <dawidchmielewski064@gmail.com>
2023-01-13 00:07:33 +0100

    Add converting JSON to list of angles

---
+++ WykryjMycha/WykryjMycha/PatternReader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Reflection.Metadata;
+using System.Globalization;
+
+namespace WykryjMycha
+{
+    public class PatternReader
+    {
+        private string jsonContent;
+
+        public PatternReader(string filepath)
+        {
+            jsonContent = File.ReadAllText(@"./src.json");
+        }
+
+        private List<double> convertStringToCoordsList(string coordinatesList)
+        {
+            List<double> coords = new List<double>();
+            String[] coordinates = coordinatesList.Split(',');
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                double currentCoord = Double.Parse(coordinates[i], CultureInfo.InvariantCulture);
+                coords.Add(currentCoord);
+            }
+            return coords;
+        }
+
+        private List<double> coordsToAngles (List<double> coords)
+        {
+            List<double> angles = new List<double>();
+
+            double newX = Convert.ToDouble(coords[0]);
+            double newY = Convert.ToDouble(coords[1]);
+
+            double oldX;
+            double oldY;
+
+            for (int i = 2; i < coords.Count; i += 2)
+            {
+                oldX = newX;
+                oldY = newY;
+
+                newX = Convert.ToDouble(coords[i]);
+                newY = Convert.ToDouble(coords[i + 1]);
+
+                double dx = newX - oldX;
+                double dy = newY - oldY;
+
+                double angle = Math.Atan2(dy, dx) * 180.0 / Math.PI;
+
+                angles.Add(angle);
+            }
+
+            return angles;
+        }
+
+        public List<double> parseToAngles()
+        {
+            var jpattern = JsonSerializer.Deserialize<JSONPattern>(jsonContent);
+
+            List<double> coordinates = convertStringToCoordsList(jpattern.coordList);
+
+            List<double> angles = coordsToAngles(coordinates);
+
+            return angles;
+        }
+    }
+
+    public class JSONPattern
+    {
+        public string name { get; set; }
+        public string coordList { get; set; }
+    }
+}
